a724018999952ff2e25170cea33c179a0af82651
Bakare Abiola <iamstarcode@gmail.com>
2023-10-09 21:30:02 +0100

    fix: fix shop post

--- src/routes/shopRoute.ts
+++ src/routes/shopRoute.ts
@@ -4,15 +4,12 @@ const router = Router();
 
 import shopController from '../controllers/shopController';
 
-router
-  .route('/')
-  .get(shopController.getAllShop)
-  .post(shopController.addShop);
+router.route('/').get(shopController.getAllShop).post(shopController.addShop);
 
 router
   .route('/:id')
-  .get(shopController.deleteShop)
+  .get(shopController.getSingleShop)
   .patch(shopController.updateShop)
-  .delete(shopController.getSingleShop);
+  .delete(shopController.deleteShop);
 
 export default router;
